a0ca9ff49b3fcbbe54baa4a3707d3f9019645b0a
Anand AttainU <anand.didwania@attainu.com>
2022-06-03 22:28:34 +0530

    asdfasdf

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ require('dotenv').config()
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.sendFile(`${__dirname}/views/index.html`);
+    res.send(process.env);
 })
 
 // app.post('/users')
 const PORT = process.env.PORT || 3000;
-console.log(process.env);
 
 
 app.listen(PORT);
